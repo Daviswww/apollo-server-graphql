41dbec48337596d8a9fffc94e1e5926178a41f56
Hsing-Wei Ho <hwho@datalab.cs.nthu.edu.tw>
2022-01-01 22:14:22 +0800

    add test

---
+++ test/env.test.js
@@ -0,0 +1,16 @@
+import { port, env, secret, db, version } from '../config/environment'
+
+test('Apollo Server', () => {
+  expect(version).not.toEqual('')
+  expect(env).not.toEqual('')
+  expect(port).not.toEqual('')
+  expect(secret).not.toEqual('')
+})
+
+test('Database', () => {
+  expect(db.host).not.toEqual('')
+  expect(db.port).not.toEqual('')
+  expect(db.name).not.toEqual('')
+  expect(db.user).not.toEqual('')
+  expect(db.password).not.toEqual('')
+})
