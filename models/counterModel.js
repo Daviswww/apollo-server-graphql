ea483561737acf816289d1881a962548aaceac01
Hsing-Wei Ho <hwho@datalab.cs.nthu.edu.tw>
2022-01-13 12:44:58 +0800

    count model

--- models/counterModel.js
+++ models/counterModel.js
@@ -1,13 +1,10 @@
-import connectDB from '../db'
-
 /* eslint-disable no-unused-vars */
 class CounterModel {
-  async selectCount (countId) {
+  async selectCount (conn, countId) {
     const _countId = countId
 
     try {
       const sql = `SELECT * FROM Count WHERE countId=${_countId};`
-      const conn = await connectDB()
       const [rows, fields] = await conn.execute(sql)
 
       return rows[0].count
@@ -16,13 +13,12 @@ class CounterModel {
     }
   }
 
-  async updateCount (args, counter) {
+  async updateCount (conn, args, counter) {
     const _countId = args.countId
     const _counter = counter
 
     try {
       const sql = `UPDATE Count SET count=${_counter} WHERE countId=${_countId};`
-      const conn = await connectDB()
       const [rows, fields] = await conn.execute(sql)
     } catch (err) {
       console.error(`[ERROR] CounterModel.updateCount :\n ${err}`)
